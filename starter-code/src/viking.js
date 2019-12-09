f0ad059b05779652318dfa9a15dbc8dca108ad14
David Aguero <22003@columbushs.com>
2019-12-09 08:43:55 -0500

    unfinished vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(num1){
+        this.health -= num1
+    }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(name)
+        this.name = name;
+        this.health = health;
+        this.strength = strength
+    }
+    receiveDamage(num1){
+        this.health -= num1;
+        if (this.health == 0){
+            return " "
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
 class Saxon {}
